6198a53b635dee8018f0a3d0af12d4ecb47c7acc
NandanaCRajeev <83021680+NandanaCRajeev@users.noreply.github.com>
2021-11-19 19:28:16 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,12 @@
+class ATM(object):
+    def __init__(self,cardnumber,pinnumber,):
+        self.cardnumber = cardnumber
+        self.pinnumber = pinnumber
+        
+
+    def CashWithDrawl(self):
+        print("CashWithDrawl")
+    def BalanceEnquiry(self):
+        print("BalanceEnquiry")
+   
+        
---
+++ ATM.py.file.py
@@ -0,0 +1,12 @@
+class Atm(object):
+    def __init__(self,card number,pin number,):
+        self.card number = card number
+        self.pin number = pin number
+        
+
+    def CashWithDrawl(self):
+        print("CashWithDrawl")
+    def BalanceEnquiry(self):
+        print("BalanceEnquiry")
+   
+        
